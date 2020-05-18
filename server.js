559d8ca04190212ba9dd27dbec64c80834a70530
thelyaa <thelyaa@yandex.ru>
2020-05-18 19:46:47 +0500

    add new files

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use('/views', express.static('views'));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get('/roles', function(req, res) {
+    res.sendFile(__dirname + '/views/roles.html');
+});
+
+app.post('/roles', urlencodedParser, function(req, res) {
+    if (!req.body) return res.sendStatus(400);
+    console.log(req.body);
+    res.sendFile(__dirname + '/views/roles.html', { data: req.body});
+});
+
+app.listen(3000);
