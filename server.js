9e4079513630994e969d9f716578010b121947c2
Roman <petessod@gmail.com>
2020-06-27 22:34:34 +0500

    added selled price

--- server.js
+++ server.js
@@ -390,6 +390,7 @@ app.post('/buyProject', function(req, res) {
     var curFirm = getFirmById(projectList[req.body.globalId].ownerFirmId);
     var curProj = projectList[req.body.globalId];
     var curPrice = req.body.price;
+    curProj.selledPrice = curPrice;
     curFirm.balance += Number(curPrice);
     customerList[req.body.customerId].budget -= curPrice;
     curProj.isSelled = true;
