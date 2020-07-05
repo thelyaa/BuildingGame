def444d4ec95aac7a8303914b349241131e2fc35
thelyaa <thelyaa@yandex.ru>
2020-07-05 15:54:40 +0500

    проверка на верный пароль

--- public/js/passwordScript.js
+++ public/js/passwordScript.js
@@ -1,5 +1,6 @@
 function EnterToGame(){
     $.post('/auth', {pass: $("#password").val()}, function(data){
-       window.location.href = "/" + data.nextPage; 
+        if (data.success)
+            window.location.href = "/" + data.nextPage; 
     });
 }
